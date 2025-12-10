6b9e9934acf3aad9a666d8ae25af526caae1afb8
Prakriti Patel <167187253+Prakriti-18@users.noreply.github.com>
2025-12-10 23:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+    this._name = name;
+    this._age = age;
+   }
+ get name() {
+    return this._name;
+  }
 
-class Student extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+	study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+	teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
